1b5fe249ebdcab8a016ee499e06403943a13289a
Afizou <afizouyayek@gmail.com>
2023-12-31 15:23:22 +0100

    debut du latest element

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Hero.css'
+import Hand_icon from '../Assets/hand_icon.png'
+import Arrow_icon from '../Assets/arrow.png'
+import Hero_img from '../Assets/hero_image.png'
 export default function Hero() {
   return (
     <div className='hero'>
-      
+      <div className='hero-left'>
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+            <div className='hand-hand-icon'>
+                <p>new</p>
+                <img src={Hand_icon} alt=''/>
+            </div>
+            <p>collections</p>
+            <p>for everyone</p>
+        </div>
+        <div className='hero-latest-btn'>
+            <div>Latest Collections</div>
+            <img src={Arrow_icon} />
+        </div>
+      </div>
+      <div className='hero-right'>
+        <img src={Hero_img}/>
+      </div>
     </div>
   )
 }
---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Popular.css'
+import data_product from '../Assets/data'
+export default function Popular() {
+  return (
+    <div className='popular'> 
+      <h1></h1>
+    </div>
+  )
+}
